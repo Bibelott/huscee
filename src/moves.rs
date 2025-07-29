0837915de76483797120eecb657b67ae1c87eb3d
BubbleBeeee <bubblebeeee@outlook.com>
2025-07-30 01:44:23 +0200

    A little bit of proc macro for my decl macro, as a treat.
    And more tests. They all pass!

--- src/moves.rs
+++ src/moves.rs
@@ -318,64 +318,53 @@ impl MoveDict {
 #[cfg(test)]
 mod tests {
     use crate::board::Board;
+    use paste::paste;
 
-    pub fn test_inner(fen: &str, max_depth: usize, expected: &[u64]) {
-        let board = Board::from_fen(fen).unwrap();
-        for depth in 1..=max_depth {
-            assert_eq!(board.perft(depth), expected[depth - 1]);
-        }
-    }
-
-    // TODO: Probably make this a proc macro so we can split each depth into a separate test
-    //
     macro_rules! test_battery {
         ($({$name:ident, $fen:expr, $expected:tt}),*) => {
             $(
-                mod $name {
-                    use super::*;
-                    #[test]
-                    fn perft() { test_inner($fen, $expected.len(), &$expected); }
-                }
+                huscee_macros::perft_test!($name, $fen, $expected);
             )*
         }
     }
 
     // NOTE: Positions taken from the Chess Programming Wiki
+    //
     test_battery!(
         {
             startpos,
             "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-            [20, 400, 8_902, 197_281]
+            [20, 400, 8_902, 197_281, 4_865_609, 119_060_324, 3_195_901_860]
         },
         {
             kiwipete,
             "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-            [48, 2039, 97_862]
+            [48, 2039, 97_862, 4_085_603, 193_690_690, 8_031_647_685]
         },
         {
             eppin,
             "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
-            [14, 191, 2_812, 43_238, 674_624]
+            [14, 191, 2_812, 43_238, 674_624, 11_030_083, 178_633_661, 3_009_794_393]
         },
         {
             checkstart,
             "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-            [6, 264, 9_467, 422_333]
+            [6, 264, 9_467, 422_333, 15_833_292, 706_045_033]
         },
         {
             checkstart_mirrored,
             "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
-            [6, 264, 9_467, 422_333]
+            [6, 264, 9_467, 422_333, 15_833_292, 706_045_033]
         },
         {
             gold,
             "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-            [44, 1_486, 62_379]
+            [44, 1_486, 62_379, 2_103_487, 89_941_194]
         },
         {
             edwards,
             "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
-            [46, 2_079, 89_890]
+            [46, 2_079, 89_890, 3_894_594, 164_075_551, 6_923_051_137]
         }
 
     );
